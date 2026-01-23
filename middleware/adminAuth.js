58df04eb3eb540e081b875ff58ce98cec645f73b
Tushar Madane <tushar@gmail.com>
2026-01-23 10:24:35 +0530

    remove old files

--- middleware/adminAuth.js
+++
@@ -1,7 +0,0 @@
-// middleware/adminAuth.js
-module.exports = function (req, res, next) {
-  if (!req.session || !req.session.admin) {
-    return res.redirect("/admin"); // redirect to login if not logged in
-  }
-  next();
-};
