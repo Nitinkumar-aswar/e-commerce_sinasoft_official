8d4a1fdb08b19d9b2c0384776d24f3ad065a4fcc
Darshan yadav <yadavdarshan066@gmail.com>
2026-02-06 15:33:46 +0530

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-echo "Starting AutoKart Backend"
+echo "Starting AutoKart Backend as ec2-user"
 
 cd /var/www/autokart/AutoKart-Backend
 
-pm2 stop autokart || true
-pm2 delete autokart || true
+sudo -u ec2-user pm2 stop autokart || true
+sudo -u ec2-user pm2 delete autokart || true
 
-pm2 start index.js --name autokart
-pm2 save
+sudo -u ec2-user pm2 start index.js --name autokart
+sudo -u ec2-user pm2 save
